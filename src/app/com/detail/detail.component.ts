df77bfe81b9d5c9962e0e0342af854e020785818
new name <nrasgari78>
2022-07-13 16:03:23 +0430

    04-22

--- src/app/com/detail/detail.component.ts
+++ src/app/com/detail/detail.component.ts
@@ -1,15 +1,14 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
-import {TodoDataService} from "../../services/todo-data.service";
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
-export class DetailComponent implements OnInit {
-  @Input() ModeEdite: boolean = false;
-  @Input() id?: number ;
+export class DetailComponent implements OnInit{
+  @Input() ModeEdite: boolean | undefined = false ;
+  @Input() selectedrecord?:any
 
   FormtTask=this.fb.group({
     title:['', [Validators.required]],
@@ -17,12 +16,8 @@ export class DetailComponent implements OnInit {
     completed:[false]
   })
 
-  loading: boolean=true;
   constructor(private fb:FormBuilder) { }
+ngOnInit() {
 
-  ngOnInit(): void {
-    // document.getElementById('title')?.focus()
-
-  }
-
+}
 }
