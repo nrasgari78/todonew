b4dfae9f6c000d935b66efa972c6f52b8cf336b4
new name <nrasgari78>
2022-07-19 14:09:33 +0430

    g

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,25 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {TaskComponent} from "./com/task/task.component";
 import {HeaderComponent} from "./com/header/header.component";
-import {AboutComponent} from "./com/about/about.component";
 
 const routes: Routes = [
   {
     path: '',
     component: HeaderComponent,
     children: [
-      {
-        path: '',
-        component: TaskComponent,
-      },
-      {
-        path: 'about',
-        component: AboutComponent,
-      },
-      {
-        path: 'task',
-        component: TaskComponent,
-      },
+      {path: '', component: TaskComponent,},
+      {path:'about',loadChildren:()=>import('./com/about/about.module').then(m=>m.AboutModule)},
+      {path: 'task', component: TaskComponent,},
 
     ]
   }
